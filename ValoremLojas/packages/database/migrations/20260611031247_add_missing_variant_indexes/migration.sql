82b181002d5380de7d10121679fcf5f1ef196187
José Silva <jose.silva@valorem.com.br>
2026-06-11 00:15:03 -0300

    fix: add missing storeId index on ProductAttribute and variantId index on VariantImage

---
+++ ValoremLojas/packages/database/migrations/20260611031247_add_missing_variant_indexes/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "product_attributes_storeId_idx" ON "product_attributes"("storeId");
+
+-- CreateIndex
+CREATE INDEX "variant_images_variantId_idx" ON "variant_images"("variantId");
